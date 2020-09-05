bee52f00f8f809d79ee35a49745a062e31a67c3e
Alexemanuelol <Alexander.emanuelsson94@gmail.com>
2020-09-05 22:15:38 +0200

    First working version of the clock and second correction

--- arduino-clock/arduino-clock.ino
+++ arduino-clock/arduino-clock.ino
@@ -1,8 +1,22 @@
+/*
+ *      Author:     Alexander Emanuelsson
+ *
+ *      Brief:
+ *
+ *      Usage:
+ */
+
+/*
+ *      Includes
+ */
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 
 #include "numbers.h"
 
+/*
+ *      Defines
+ */
 #define CLK_PIN     13 /* SCK */
 #define DATA_PIN    11 /* MOSI */
 #define CS_PIN      10 /* SS */
@@ -10,43 +24,47 @@
 #define MAX_DEVICES     4
 #define MATRIX_WIDTH    (8 * MAX_DEVICES)
 #define MATRIX_HEIGHT   8
-#define CHAT_SPACING    1
 
 #define HOURS_START_POS     0
 #define MINUTES_START_POS   12
 #define SECONDS_START_POS   24
 
-int timer1_counter;
+#define SECOND_CORRECTION_MINUTES   106
 
-enum numberSize {
+
+/*
+ *      Enums
+ */
+typedef enum {
     SMALL = 0,
     BIG
-};
+} numberSize;
 
 typedef enum {
     STANDBY = 0,
     UPDATE
 } appState;
 
+
+/*
+ *      Global variables
+ */
 MD_MAX72XX mx = MD_MAX72XX(MD_MAX72XX::PAROLA_HW, CS_PIN, MAX_DEVICES);
 
 uint8_t seconds = 0;
 uint8_t minutes = 0;
 uint8_t hours = 0;
+uint8_t minuteCounter = 0;
+bool alternateColon = 0;
+
 static appState state = STANDBY;
-bool alternate = 0;
 
-
-/* Interrupt Service Routine */
-ISR(TIMER1_OVF_vect)
-{
-    //TCNT1 = timer1_counter;
-    state = UPDATE;
-}
-
+/*
+ *      Main function
+ */
 int main(void)
 {
-    initInterrupts();
+    initInterrupt();
     mx.begin();
 
     set_hours(hours);
@@ -61,6 +79,9 @@ int main(void)
     return 0;
 }
 
+/*
+ *      application function
+ */
 void app()
 {
     switch (state)
@@ -68,61 +89,79 @@ void app()
         case STANDBY:
             mx.update();
             break;
-            
+
         case UPDATE:
             state = STANDBY;
+
             seconds = seconds + 1;
             if (seconds == 60)
             {
                 seconds = 0;
                 minutes = minutes + 1;
+                minuteCounter = minuteCounter + 1;
+                if (minuteCounter == SECOND_CORRECTION_MINUTES)
+                {
+                    minuteCounter = 0;
+                    seconds = seconds + 1;
+                }
+                if (minutes == 60)
+                {
+                    minutes = 0;
+                    hours = hours + 1;
+                    if (hours == 24)
+                    {
+                        hours = 0;
+                    }
+                    set_hours(hours);
+                }
                 set_minutes(minutes);
             }
-            if (minutes == 60)
+            set_seconds(seconds);
+
+            if (alternateColon)
             {
-                minutes = 0;
-                hours = hours + 1;
-                set_hours(hours);
-            }
-            if (hours == 24)
-            {
-                hours = 0;
-            }
-            
-            if (alternate)
-            {
-                alternate = 0;
+                alternateColon = 0;
                 mx.setColumn(10, 0x14);
             }
             else
             {
-                alternate = 1;
+                alternateColon = 1;
                 mx.setColumn(10, 0x00);
             }
-            set_seconds(seconds);
             break;
-            
+
         default:
             break;
     }
 }
 
-void initInterrupts()
+/*
+ *      Interrupt Service Routine
+ */
+ISR(TIMER1_OVF_vect)
+{
+    state = UPDATE;
+}
+
+/*
+ *      Interrupt initialization
+ */
+void initInterrupt()
 {
     noInterrupts();     /* Disable all interrupts */
     TCCR1A = 0x00;
     TCCR1B = 0x00;
-
-    //timer1_counter = 34286;     /* Preload timer 65536-16MHz/256/2Hz */
-
-    TCNT1 = 0x0000;//timer1_counter;     /* Preload timer */
-    TCCR1A = 0xA2; // fast 16 bit PWM
-    TCCR1B = 0x1c;//|= (1 << CS12);      /* 256 Prescaler */
+    TCNT1 = 0x0000;     /* Preload timer */
+    TCCR1A = 0xA2;      /* fast 16 bit PWM */
+    TCCR1B = 0x1c;      /* 256 Prescaler */
     ICR1 = 62500-1;
-    TIMSK1 |= 0x01;//(1 << TOIE1);     /* Enable timer overflow interrupt */
-    interrupts();               /* Enable all interrupts */
+    TIMSK1 |= 0x01;     /* Enable timer overflow interrupt */
+    interrupts();       /* Enable all interrupts */
 }
 
+/*
+ *      Print a number on the display
+ */
 void set_number(uint8_t startPos, uint8_t number, numberSize numSize)
 {
     switch (numSize)
@@ -143,18 +182,27 @@ void set_number(uint8_t startPos, uint8_t number, numberSize numSize)
     }
 }
 
+/*
+ *      Set a number at the hours position
+ */
 void set_hours(uint8_t hours)
 {
     set_number(HOURS_START_POS, (uint8_t) hours / 10, BIG);
     set_number(HOURS_START_POS + 5, (uint8_t) hours % 10, BIG);
 }
 
+/*
+ *      Set a number at the minutes position
+ */
 void set_minutes(uint8_t minutes)
 {
     set_number(MINUTES_START_POS, (uint8_t) minutes / 10, BIG);
     set_number(MINUTES_START_POS + 5, (uint8_t) minutes % 10, BIG);
 }
 
+/*
+ *      Set a number at the seconds position
+ */
 void set_seconds(uint8_t seconds)
 {
     set_number(SECONDS_START_POS, (uint8_t) seconds / 10, SMALL);
